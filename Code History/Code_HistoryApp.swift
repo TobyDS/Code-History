e304f8a1cd935ed1aa29b98f057b92c766aff91e
Toby Dixon Smith <tobydixonsmith@gmail.com>
2024-06-25 21:51:13 +0100

    initial commit

---
+++ Code History/Code_HistoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  Code_HistoryApp.swift
+//  Code History
+//
+//  Created by Toby Dixon Smith on 25/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct Code_HistoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
